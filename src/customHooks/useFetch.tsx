736575214fe1c0fbff93509194dd830b2d9a76ae
Tigran <mr.tigran.aslanyan@gmail.com>
2025-05-13 11:55:16 +0400

    Added .env for the Api key

--- src/customHooks/useFetch.tsx
+++ src/customHooks/useFetch.tsx
@@ -1,29 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
+const useFetch = (userInput: string) => {
+  const [data, setData] = useState<any[]>([]);
 
+  useEffect(() => {
+    const API_KEY = import.meta.env.VITE_KEY; 
+    console.log('▶︎ using key:', API_KEY);
 
-
-const useFetch = (userInput) => {
-
-  const [data, setData] = useState([]);
-useEffect(() => {
-  
-  const API_KEY = "3e2bc978b19b61c296bbfd833ff9ecd5";
-  const url = userInput.trim()
-      ? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(userInput)}`
+    const url = userInput.trim()
+      ? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${userInput}`
       : `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
     fetch(url)
-    .then((data) =>  data.json())
-    .then((res) => setData(res.results || []))
-    .catch(err => console.log(err));
+      .then(r => r.json())
+      .then(res => setData(res.results || []))
+      .catch(err => console.error(err));
+  }, [userInput]);
 
-}, [userInput])
-
-   return data;
-}
+  return data;
+};
 
 export default useFetch;
-
-
-
